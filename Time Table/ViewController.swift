eb3d7e65857c3f5b3a465be957a30afa6a07672f
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:38:18 +0530

    Time Table
    A simple numbers table app

--- Time Table/ViewController.swift
+++ Time Table/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    var rows = 20
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderAction(sender: AnyObject) {
+        
+        displayLabel.text = String(Int(sliderValue.value))
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return rows
+    
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timesTable = String(Int(sliderValue.value) * (indexPath.row + 1))
+        
+        cell.textLabel?.text = "\(String(Int(sliderValue.value))) x \(indexPath.row + 1) = \(timesTable)"
+        
+        return cell
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        displayLabel.text = String(Int(sliderValue.value))
     }
 
     override func didReceiveMemoryWarning() {
